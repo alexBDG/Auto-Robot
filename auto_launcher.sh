c46bad5d136a5c94ba7dd4af2001efa68364d5c2
alexBDG <34397808+alexBDG@users.noreply.github.com>
2023-09-23 17:58:31 +0200

    Fix bad commands into auto_launcher, add error redirection

--- auto_launcher.sh
+++ auto_launcher.sh
@@ -5,5 +5,5 @@ LOG_DIR=$PROJECT_DIR/auto_logs
 SERVER_DIR=$PROJECT_DIR/Auto-Robot/autorobot/server
 
 cd $SERVER_DIR
-$PYTHON_EXE server_camera.py --fps 30 &> $LOG_DIR/camera.log
-$PYTHON_EXE server_commands.py &> $LOG_DIR/commands.log
+nohup $PYTHON_EXE server_camera.py --fps 30 > $LOG_DIR/camera.log 2>&1 &
+nohup $PYTHON_EXE server_commands.py &> $LOG_DIR/commands.log 2>&1 &
